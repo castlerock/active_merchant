01b7eb0d3477831023e0c65a1a44017cf0fa8d3c
Sukeerthi Adiga <sukeerthiadiga@gmail.com>
2018-03-12 15:50:04 +0530

    setting ssl as nil

--- lib/active_merchant/common/connection.rb
+++ lib/active_merchant/common/connection.rb
@@ -110,7 +110,7 @@ module ActiveMerchant
       return unless endpoint.scheme == "https"
 
       http.use_ssl = true
-      http.ssl_version = :TLSv1_2
+      http.ssl_version = nil
 
       if verify_peer
         http.verify_mode = OpenSSL::SSL::VERIFY_PEER
